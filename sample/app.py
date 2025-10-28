7c1820ab7c267a987005547e3b8906f2f25043f4
Raamsrupesh <yerrarupesh756@gmail.com>
2025-10-28 08:02:31 +0530

    Update app.py

--- sample/app.py
+++ sample/app.py
@@ -76,7 +76,7 @@ else:
     </style>
     """, unsafe_allow_html=True)
     st.sidebar.image("https://icon2.cleanpng.com/20180424/vdq/avttdstoo.webp", width=100)
-    st.sidebar.title("**Presents**")
+    # st.sidebar.title("**Presents**")
     page = st.sidebar.radio("Select either of these:", ["Mentor", "Student", "About", "Settings"], index=1)
     import html
     if page == "Mentor":
@@ -830,6 +830,7 @@ else:
                 st.error("❌❌ Error Occured!!")
                 # password_df=pd.concat([password_df, pd.DataFrame({'device_id': device_id})])
     
+
 
 
 
