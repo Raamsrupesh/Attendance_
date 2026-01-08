135ebea049295a524f05b71ad0a7d3f12e757578
Raamsrupesh <yerrarupesh756@gmail.com>
2026-01-08 07:31:17 +0530

    Update app.py

--- sample/app.py
+++ sample/app.py
@@ -439,12 +439,13 @@ else:
                 # total_days from above expander scope; you may want to move this calculation earlier safely
                 try:
                     percentage = total_present_days
-                    if percentage < 62:
-                        st.error(f"📉 The attendance percentage is: {percentage}%")
-                    elif percentage >= 63 and percentage < 75:
-                        st.warning(f"📊 The attendance percentage is: {percentage}%")
-                    else:
-                        st.success(f"📈 The attendance percentage is: {percentage}%")
+                    st.write(percentage)
+                    # if percentage < 62:
+                    #     st.error(f"📉 The attendance percentage is: {percentage}%")
+                    # elif percentage >= 63 and percentage < 75:
+                    #     st.warning(f"📊 The attendance percentage is: {percentage}%")
+                    # else:
+                    #     st.success(f"📈 The attendance percentage is: {percentage}%")
                 except Exception:
                     st.error("Contact Admin")
 
@@ -1137,6 +1138,7 @@ else:
 
 # import streamlit as st
 # st.write(pd.concat([df1, df1['Name'].isin(df2['appeleation'])], axis=1, ignore_index=True))
+
 
 
 
